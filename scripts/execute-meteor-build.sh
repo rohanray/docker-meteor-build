86a39755520b17cb67a08dca40931e98e4cecbdc
Rohan Ray <rohanray@gmail.com>
2016-08-16 12:17:46 +0000

    tar output instead of directory

--- scripts/execute-meteor-build.sh
+++ scripts/execute-meteor-build.sh
@@ -5,8 +5,8 @@ apt-get -qq update
 apt-get -qq install -y python make g++
 cd /opt/src
 meteor npm install
-meteor build --directory /opt/dist --architecture os.linux.x86_64 --verbose
-cd /opt/dist/bundle
+meteor build /opt/dist --architecture os.linux.x86_64 --verbose
+cd /opt/dist
 ls -la
 rm /usr/local/bin/meteor && rm -rf ~/.meteor /opt/src /meteor-docker-scripts
 apt-get -qq purge -y python make g++
