c141ecd518dfa5c1cc53558268325cca955d35e0
Rohan Ray <rohanray@gmail.com>
2016-08-16 08:55:27 +0000

    DONE!

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ MAINTAINER    Rohan Ray (https://github.com/rohanray)
 COPY scripts /meteor-docker-scripts
 RUN sh /meteor-docker-scripts/main-script.sh
 
-ENTRYPOINT ["sh", "/meteor-docker-scripts/execute-meteor-build.sh"]
+ENTRYPOINT ["echo", "Node app built and ready at /opt/dist"]
 
 ONBUILD COPY  ./ /opt/src
+ONBUILD RUN sh /meteor-docker-scripts/execute-meteor-build.sh
--- scripts/execute-meteor-build.sh
+++ scripts/execute-meteor-build.sh
@@ -2,11 +2,13 @@
 
 echo "[+5+] START: Executing meteor build command"
 apt-get -qq update 
-apt-get -q install -y python make g++
+apt-get -qq install -y python make g++
 cd /opt/src
 meteor npm install
 meteor build --directory /opt/dist --architecture os.linux.x86_64 --verbose
-rm /usr/local/bin/meteor && rm -rf ~/.meteor
-apt-get -q purge -y python make g++
-apt-get -q remove --purge -y python make g++
+cd /opt/dist/bundle
+ls -la
+rm /usr/local/bin/meteor && rm -rf ~/.meteor /opt/src /meteor-docker-scripts
+apt-get -qq purge -y python make g++
+apt-get -qq remove --purge -y python make g++
 echo "[+5+] FIN: Executing meteor build command"
