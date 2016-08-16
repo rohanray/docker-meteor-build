d761edab80ea0f2bc5fa319b7cf9f672fd0b6d46
Rohan Ray <rohanray@gmail.com>
2016-08-16 07:05:31 +0000

    install python make g++ during meteor build phase

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM          debian:jessie
 MAINTAINER    Rohan Ray (https://github.com/rohanray)
 
 COPY scripts /meteor-docker-scripts
-RUN ls -la /meteor-docker-scripts
 RUN sh /meteor-docker-scripts/main-script.sh
 
 ENTRYPOINT ["sh", "/meteor-docker-scripts/execute-meteor-build.sh"]
--- scripts/execute-meteor-build.sh
+++ scripts/execute-meteor-build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 echo "[+5+] START: Executing meteor build command"
-meteor npm install /opt/src
-meteor build --directory /opt/dist --architecture os.linux.x86_64
+apt-get -qq update 
+apt-get -q install -y python make g++
+cd /opt/src
+meteor npm install
+meteor build --directory /opt/dist --architecture os.linux.x86_64 --verbose
+rm /usr/local/bin/meteor && rm -rf ~/.meteor
+apt-get -q purge -y python make g++
+apt-get -q remove --purge -y python make g++
 echo "[+5+] FIN: Executing meteor build command"
